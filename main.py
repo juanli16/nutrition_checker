c9c587098773400a3feb618c5b857bb84a8befc8
justin <juan.y.li@mail.mcgill.ca>
2026-04-02 23:02:33 -0400

    add dataset

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+from pathlib import Path
 import torch
+from torchvision import datasets
+from model.helpers import plot
+import matplotlib.pyplot as plt
+
+ROOT = Path("data/nutritionverse-real/nutritionverse-manual/nutritionverse-manual")
+IMAGE_PATH = str(ROOT / "images")
+ANNOTATION_PATH = str(ROOT / "images" / "_annotations.coco.json")
 
 def main():
-    print("Hello from nutrition-checker!")
-    x = torch.rand(5, 3)
-    print(x)
+    ds = datasets.CocoDetection(IMAGE_PATH, ANNOTATION_PATH)
+
+    img, target = ds[0]
+    print(target[0].keys())
+
+    dataset = datasets.wrap_dataset_for_transforms_v2(ds, target_keys=("boxes", "labels", "masks"))
+
+    print(dataset[0])
+    plot([dataset[0], dataset[1]])
+    plt.show()
+
 
 
 if __name__ == "__main__":
